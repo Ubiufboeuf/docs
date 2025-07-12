ab7897f49b29b7c24cb8b451a09e90c5f8a11bfa
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-07-12 15:45:51 -0300

    feat: añadir ruta para aprender en el menú lateral

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,8 +10,21 @@ export default defineConfig({
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/Ubiufboeuf/docs' }],
 			sidebar: [
 				{
-					label: 'Aprender',
-					autogenerate: { directory: 'aprender' }
+					label: 'Recomendado',
+					autogenerate: { directory: 'recomendado' }
+				},
+				{
+					label: 'Ruta para aprender',
+					items: [
+						{ slug: 'ruta-para-aprender', label: 'General' },
+						{ slug: 'ruta-para-aprender/html', label: 'HTML' },
+						{ slug: 'ruta-para-aprender/css', label: 'CSS' },
+						{ slug: 'ruta-para-aprender/js', label: 'JavaScript' },
+						{ slug: 'ruta-para-aprender/astro', label: 'Astro' },
+						{ slug: 'ruta-para-aprender/tailwind', label: 'TailwindCSS' },
+						{ slug: 'ruta-para-aprender/preact', label: 'Preact' },
+						{ slug: 'ruta-para-aprender/ts', label: 'TypeScript' },
+					]
 				},
 				{
 					label: 'Glosario',
